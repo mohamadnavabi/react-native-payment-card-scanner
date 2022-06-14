7336dc514f627fc45ed507a968d58e75e77e82e6
Mohammad Navabi <7navabi@gmail.com>
2022-06-14 13:33:43 +0430

    Update PaymentCardScanner.m

--- ios/PaymentCardScanner.m
+++ ios/PaymentCardScanner.m
@@ -4,4 +4,9 @@
 
 RCT_EXTERN_METHOD(scan: (NSString*)topText withBottomText: (NSString*)topText withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
++ (BOOL)requiresMainQueueSetup
+{
+  return NO;
+}
+
 @end
